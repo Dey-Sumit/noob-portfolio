e7420f0aaa1af9a8651145d6a39d38c05d2e0a42
unknown <sumax333@gmail.com>
2020-02-05 19:20:53 +0530

    basic skeleton of the app

---
+++ index.js
@@ -0,0 +1,19 @@
+//import the module
+const express = require('express');
+
+//create an express object
+const app = express();
+
+//listen to a PORT
+app.listen(3000, () => console.log("listening on 3000"));
+
+//set up the application middleware using app.use()
+/*Middleware functions are functions that have access to the request object (req),
+  the response object (res), and the next middleware function in the application’s request-response cycle.*/
+app.use((req, res, next) => {
+  console.log(`${req.method} for ${req.url}`);
+  next();
+});
+
+//host the static folder
+app.use(express.static('public'));
